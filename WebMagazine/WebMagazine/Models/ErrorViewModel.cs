fabfd47b9bc040d5ced7608e47ef52ea7d11a2d8
Leonardo Azenha <Azenha@users.noreply.github.com>
2022-09-26 19:22:24 -0300

    add projeto aula WebMagazine

---
+++ WebMagazine/WebMagazine/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebMagazine.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
