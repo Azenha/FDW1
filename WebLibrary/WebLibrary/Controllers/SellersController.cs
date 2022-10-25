b32a13accc2f4f5b3a3c76da47f0f3b66653b374
Leonardo Azenha <Azenha@users.noreply.github.com>
2022-10-24 21:52:26 -0300

    aula 20221024

--- WebLibrary/WebLibrary/Controllers/SellersController.cs
+++ WebLibrary/WebLibrary/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using WebLibrary.Models;
 using WebLibrary.Models.ViewModels;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using NuGet.Protocol.Plugins;
 
 namespace WebLibrary.Controllers
 {
@@ -17,9 +18,16 @@ namespace WebLibrary.Controllers
 
         public IActionResult Index()
         {
-            var list = _context.Seller.Include(d => d.Department).ToList();
+            //var list = _context.Seller.Include(d => d.Department).ToList();
             //é igual à
             //List<Seller> list = new List<Seller>();
+            //var list = _context.Seller.Include(s => s.Department).Where(s => s.Name.StartsWith("A")).ToList();
+            //var list = _context.Seller.Include(s => s.Department).Where(s => s.Name.EndsWith("a")).ToList();
+            //var list = _context.Seller.Include(s => s.Department).Where(s => s.Name.Contains("Azenha")).ToList();
+            //var list = _context.Seller.Include(s => s.Department).Where(s => s.Name == "Azenha").ToList();
+            //var list = _context.Seller.Include(s => s.Department).Where(s => s.Salary >2000).ToList();
+            //var list = _context.Seller.Include(s => s.Department).Where(s => s.Name.Contains("Vendedor")).OrderBy(s => s.Name).ThenBy(s => s.Salary).ToList();
+            var list = _context.Seller.Include(s => s.Department).Where(s => s.BirthDate > new DateTime(1998, 1, 1)).OrderBy(s => s.Name).ThenBy(s => s.Salary).ToList();
             return View(list);
         }
 
@@ -73,6 +81,30 @@ namespace WebLibrary.Controllers
                 return NotFound();
             }
             _context.Seller.Remove(seller);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Seller seller = _context.Seller.Include(s => s.Department).FirstOrDefault(s => s.Id == id);
+            if(seller == null)
+            {
+                return NotFound();
+            }
+            var viewModel = new SellerFormViewModel();
+            viewModel.Seller = seller;
+            viewModel.Departments = _context.Department.ToArray();
+            return View(viewModel);
+        }
+        [HttpPost]
+        public IActionResult Edit(Seller seller)
+        {
+            _context.Seller.Update(seller);
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
--- WebLibrary/WebLibrary/Models/Seller.cs
+++ WebLibrary/WebLibrary/Models/Seller.cs
@@ -5,12 +5,19 @@ namespace WebLibrary.Models
     public class Seller
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Campo Obrigatório!")]
+        [MinLength(3, ErrorMessage = "O Zé era inválido"),MaxLength(28, ErrorMessage = "o limite de caractéres é igual a inconstitucionalissimamente.")]
         public string Name { get; set; }
         [Display(Name = "E-mail")]
+        [EmailAddress(ErrorMessage = "Digite um e-mail válido")]
         public string Email { get; set; }
         [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
         public double Salary { get; set; }
+        [Display(Name = "Department")]
         public int DepartmentId { get; set; }
         public Department Department { get; set; }
         public ICollection<SalesRecord> Sales = new List<SalesRecord>();
