9e05f5f07e4b9708c546b0de2ed30f324c8ffeca
Leonardo Azenha <Azenha@users.noreply.github.com>
2022-11-07 22:01:10 -0300

    Corrigir erro de update-database

---
+++ NpcManager/NpcManager/Migrations/20221108005327_Primeira.cs
@@ -0,0 +1,351 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NpcManager.Migrations
+{
+    public partial class Primeira : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Npc_Autenticidade_AutenticidadeDoisId",
+                table: "Npc");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Npc_Autenticidade_AutenticidadeTresId",
+                table: "Npc");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Npc_Autenticidade_AutenticidadeUmId",
+                table: "Npc");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Npc_Experiencia_ExperienciaDoisId",
+                table: "Npc");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Npc_Experiencia_ExperienciaUmId",
+                table: "Npc");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Npc_Origem_OrigemId",
+                table: "Npc");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Origem",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Retrato",
+                table: "Npc",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OrigemId",
+                table: "Npc",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ExperienciaUmId",
+                table: "Npc",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ExperienciaDoisId",
+                table: "Npc",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Npc",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AutenticidadeUmId",
+                table: "Npc",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AutenticidadeTresId",
+                table: "Npc",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AutenticidadeDoisId",
+                table: "Npc",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Experiencia",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Autenticidade",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Npc_Autenticidade_AutenticidadeDoisId",
+                table: "Npc",
+                column: "AutenticidadeDoisId",
+                principalTable: "Autenticidade",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Npc_Autenticidade_AutenticidadeTresId",
+                table: "Npc",
+                column: "AutenticidadeTresId",
+                principalTable: "Autenticidade",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Npc_Autenticidade_AutenticidadeUmId",
+                table: "Npc",
+                column: "AutenticidadeUmId",
+                principalTable: "Autenticidade",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Npc_Experiencia_ExperienciaDoisId",
+                table: "Npc",
+                column: "ExperienciaDoisId",
+                principalTable: "Experiencia",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Npc_Experiencia_ExperienciaUmId",
+                table: "Npc",
+                column: "ExperienciaUmId",
+                principalTable: "Experiencia",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Npc_Origem_OrigemId",
+                table: "Npc",
+                column: "OrigemId",
+                principalTable: "Origem",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Npc_Autenticidade_AutenticidadeDoisId",
+                table: "Npc");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Npc_Autenticidade_AutenticidadeTresId",
+                table: "Npc");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Npc_Autenticidade_AutenticidadeUmId",
+                table: "Npc");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Npc_Experiencia_ExperienciaDoisId",
+                table: "Npc");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Npc_Experiencia_ExperienciaUmId",
+                table: "Npc");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Npc_Origem_OrigemId",
+                table: "Npc");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Origem",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Retrato",
+                table: "Npc",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OrigemId",
+                table: "Npc",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ExperienciaUmId",
+                table: "Npc",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ExperienciaDoisId",
+                table: "Npc",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Npc",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AutenticidadeUmId",
+                table: "Npc",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AutenticidadeTresId",
+                table: "Npc",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AutenticidadeDoisId",
+                table: "Npc",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Experiencia",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Autenticidade",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Npc_Autenticidade_AutenticidadeDoisId",
+                table: "Npc",
+                column: "AutenticidadeDoisId",
+                principalTable: "Autenticidade",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Npc_Autenticidade_AutenticidadeTresId",
+                table: "Npc",
+                column: "AutenticidadeTresId",
+                principalTable: "Autenticidade",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Npc_Autenticidade_AutenticidadeUmId",
+                table: "Npc",
+                column: "AutenticidadeUmId",
+                principalTable: "Autenticidade",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Npc_Experiencia_ExperienciaDoisId",
+                table: "Npc",
+                column: "ExperienciaDoisId",
+                principalTable: "Experiencia",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Npc_Experiencia_ExperienciaUmId",
+                table: "Npc",
+                column: "ExperienciaUmId",
+                principalTable: "Experiencia",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Npc_Origem_OrigemId",
+                table: "Npc",
+                column: "OrigemId",
+                principalTable: "Origem",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- NpcManager/NpcManager/Models/Npc.cs
+++ NpcManager/NpcManager/Models/Npc.cs
@@ -4,14 +4,14 @@
     {
         public int Id { get; set; }
         public string Nome { get; set; }
-        public string Descricao { get; set; }
-        public string Retrato { get; set; }
-        public Origem Origem { get; set; }
-        public Experiencia ExperienciaUm { get; set; }
-        public Experiencia ExperienciaDois { get; set; }
-        public Autenticidade AutenticidadeUm { get; set; }
-        public Autenticidade AutenticidadeDois { get; set; }
-        public Autenticidade AutenticidadeTres { get; set; }
+        public string? Descricao { get; set; }
+        public string? Retrato { get; set; }
+        public Origem? Origem { get; set; }
+        public Experiencia? ExperienciaUm { get; set; }
+        public Experiencia? ExperienciaDois { get; set; }
+        public Autenticidade? AutenticidadeUm { get; set; }
+        public Autenticidade? AutenticidadeDois { get; set; }
+        public Autenticidade? AutenticidadeTres { get; set; }
 
         public Npc() { }
         public Npc(int id, string nome, string descricao, string retrato, Origem origem, Experiencia experienciaUm, Experiencia experienciaDois, Autenticidade autenticidadeUm, Autenticidade autenticidadeDois, Autenticidade autenticidadeTres)
