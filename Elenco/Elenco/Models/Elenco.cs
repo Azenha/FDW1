5018845283a42a1f1ffa07ec8705d52a6e4676d6
Leonardo Azenha <Azenha@users.noreply.github.com>
2022-11-21 19:32:15 -0300

    Agora vai

--- Elenco/Elenco/Models/Elenco.cs
+++ Elenco/Elenco/Models/Elenco.cs
@@ -1,4 +1,6 @@
-﻿namespace Elenco.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Elenco.Models
 {
     public class Elenco
     {
@@ -7,31 +9,32 @@
         public string? Descricao { get; set; }
         public string? Retrato { get; set; }
         public int? Origem { get; set; }
-        public int? ExperienciaAlfaId { get; set; }
-        public int? ExperienciaBetaId { get; set; }
-        public int? AutenticidadeAlfaId { get; set; }
-        public int? AutenticidadeBetaId { get; set; }
-        public int? AutenticidadeGamaId { get; set; }
-        public virtual Experiencia ExperienciaAlfa { get; set; }
-        public virtual Experiencia ExperienciaBeta { get; set; }
-        public virtual Autenticidade AutenticidadeAlfa { get; set; }
-        public virtual Autenticidade AutenticidadeBeta { get; set; }
-        public virtual Autenticidade AutenticidadeGama { get; set; }
+        [InverseProperty("ExperienciaAlfa")]
+        public virtual Experiencia? ExperienciaAlfa { get; set; }
+        [InverseProperty("ExperienciaBeta")]
+        public virtual Experiencia? ExperienciaBeta { get; set; }
+        [InverseProperty("AutenticidadeAlfa")]
+        public virtual Autenticidade? AutenticidadeAlfa { get; set; }
+        [InverseProperty("AutenticidadeBeta")]
+        public virtual Autenticidade? AutenticidadeBeta { get; set; }
+        [InverseProperty("AutenticidadeGama")]
+        public virtual Autenticidade? AutenticidadeGama { get; set; }
 
-        //public Origem? Origem { get; set; }
-        //public Experiencia? Experiencia { get; set; }
-        //public Autenticidade? Autenticidade { get; set; }
         public Elenco() { }
 
-        //public Elenco(int id, string nome, string? descricao, string? retrato, Origem? origem, Experiencia? experiencia, Autenticidade? autenticidade)
-        //{
-        //    Id = id;
-        //    Nome = nome;
-        //    Descricao = descricao;
-        //    Retrato = retrato;
-        //    Origem = origem;
-        //    Experiencia = experiencia;
-        //    Autenticidade = autenticidade;
-        //}
+        public Elenco(int id, string nome, string? descricao, string? retrato, int? origem, Experiencia? experienciaAlfa, Experiencia? experienciaBeta, Autenticidade? autenticidadeAlfa, Autenticidade? autenticidadeBeta, Autenticidade? autenticidadeGama)
+        {
+            Id = id;
+            Nome = nome;
+            Descricao = descricao;
+            Retrato = retrato;
+            Origem = origem;
+            ExperienciaAlfa = experienciaAlfa;
+            ExperienciaBeta = experienciaBeta;
+            AutenticidadeAlfa = autenticidadeAlfa;
+            AutenticidadeBeta = autenticidadeBeta;
+            AutenticidadeGama = autenticidadeGama;
+        }
+
     }
 }
