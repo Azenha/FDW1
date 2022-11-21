86f486eb3f3796982099cb0ac259bed9ca567d41
Leonardo Azenha <Azenha@users.noreply.github.com>
2022-11-21 20:19:05 -0300

    FOI

---
+++ Elenco/Elenco/Controllers/CoadjuvantesController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Elenco.Data;
+using Elenco.Models;
+
+namespace Elenco.Controllers
+{
+    public class CoadjuvantesController : Controller
+    {
+        private readonly ElencoContext _context;
+
+        public CoadjuvantesController(ElencoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Coadjuvantes
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Elenco.ToListAsync());
+        }
+
+        // GET: Coadjuvantes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Elenco == null)
+            {
+                return NotFound();
+            }
+
+            var coadjuvante = await _context.Elenco
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (coadjuvante == null)
+            {
+                return NotFound();
+            }
+
+            return View(coadjuvante);
+        }
+
+        // GET: Coadjuvantes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Coadjuvantes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Descricao,Retrato")] Coadjuvante coadjuvante)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(coadjuvante);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(coadjuvante);
+        }
+
+        // GET: Coadjuvantes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Elenco == null)
+            {
+                return NotFound();
+            }
+
+            var coadjuvante = await _context.Elenco.FindAsync(id);
+            if (coadjuvante == null)
+            {
+                return NotFound();
+            }
+            return View(coadjuvante);
+        }
+
+        // POST: Coadjuvantes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Descricao,Retrato")] Coadjuvante coadjuvante)
+        {
+            if (id != coadjuvante.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(coadjuvante);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CoadjuvanteExists(coadjuvante.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(coadjuvante);
+        }
+
+        // GET: Coadjuvantes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Elenco == null)
+            {
+                return NotFound();
+            }
+
+            var coadjuvante = await _context.Elenco
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (coadjuvante == null)
+            {
+                return NotFound();
+            }
+
+            return View(coadjuvante);
+        }
+
+        // POST: Coadjuvantes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Elenco == null)
+            {
+                return Problem("Entity set 'ElencoContext.Elenco'  is null.");
+            }
+            var coadjuvante = await _context.Elenco.FindAsync(id);
+            if (coadjuvante != null)
+            {
+                _context.Elenco.Remove(coadjuvante);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CoadjuvanteExists(int id)
+        {
+          return _context.Elenco.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Elenco/Elenco/Migrations/20221121231731_alfa.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Elenco.Migrations
+{
+    public partial class alfa : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Autenticidade",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autenticidade", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Experiencia",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Experiencia", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Origem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Origem", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Elenco",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Retrato = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OrigemId = table.Column<int>(type: "int", nullable: true),
+                    ExperienciaAlfaId = table.Column<int>(type: "int", nullable: true),
+                    ExperienciaBetaId = table.Column<int>(type: "int", nullable: true),
+                    AutenticidadeAlfaId = table.Column<int>(type: "int", nullable: true),
+                    AutenticidadeBetaId = table.Column<int>(type: "int", nullable: true),
+                    AutenticidadeGamaId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Elenco", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Elenco_Autenticidade_AutenticidadeAlfaId",
+                        column: x => x.AutenticidadeAlfaId,
+                        principalTable: "Autenticidade",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Elenco_Autenticidade_AutenticidadeBetaId",
+                        column: x => x.AutenticidadeBetaId,
+                        principalTable: "Autenticidade",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Elenco_Autenticidade_AutenticidadeGamaId",
+                        column: x => x.AutenticidadeGamaId,
+                        principalTable: "Autenticidade",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Elenco_Experiencia_ExperienciaAlfaId",
+                        column: x => x.ExperienciaAlfaId,
+                        principalTable: "Experiencia",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Elenco_Experiencia_ExperienciaBetaId",
+                        column: x => x.ExperienciaBetaId,
+                        principalTable: "Experiencia",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Elenco_Origem_OrigemId",
+                        column: x => x.OrigemId,
+                        principalTable: "Origem",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Elenco_AutenticidadeAlfaId",
+                table: "Elenco",
+                column: "AutenticidadeAlfaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Elenco_AutenticidadeBetaId",
+                table: "Elenco",
+                column: "AutenticidadeBetaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Elenco_AutenticidadeGamaId",
+                table: "Elenco",
+                column: "AutenticidadeGamaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Elenco_ExperienciaAlfaId",
+                table: "Elenco",
+                column: "ExperienciaAlfaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Elenco_ExperienciaBetaId",
+                table: "Elenco",
+                column: "ExperienciaBetaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Elenco_OrigemId",
+                table: "Elenco",
+                column: "OrigemId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Elenco");
+
+            migrationBuilder.DropTable(
+                name: "Autenticidade");
+
+            migrationBuilder.DropTable(
+                name: "Experiencia");
+
+            migrationBuilder.DropTable(
+                name: "Origem");
+        }
+    }
+}
